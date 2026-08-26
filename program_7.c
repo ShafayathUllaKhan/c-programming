149c2c9c8546da99e3c3324b239912507fe04348
shafayath <shafayathullakhan1998@gmail.com>
2026-08-27 01:08:13 +0530

    lateast commit

--- program_7.c
+++ program_7.c
@@ -2,6 +2,12 @@
 
 int main(void)
 {
-    
+    char *canChange = "Hello World";
+    int a = 10;
+    printf("%p\n",canChange); // ask for address
+    printf("%s\n",canChange); // ask for address and print full string
+    printf("%c\n",*canChange); // ask for value
+    printf("%d\n",a); // ask for value
+    printf("%d\n",*canChange);// prints 72 beacuse %d will look for integer and h ascii value is 72
     return 0;
 }
--- program_8.c
+++ program_8.c
@@ -2,6 +2,29 @@
 
 int main(void)
 {
-    
+    char *string_v = "Hello World";
+
+    // this works perfectly because string_v is an address
+    // puts() function writes a string to the output screen and automatically adds a newline character (\0 to \n) at the end
+    // puts expects address
+    puts(string_v);
+
+    // it is exactly equivalent to:
+    // printf("%s\n", string_v);
+
+    // A buffer that can only hold 9 characters plus the null terminator (\0)
+    char buffer[10];
+
+    printf("Enter your name:\n");
+
+    // Dangerous: reads input until you press enter, ignoring the size limit
+    // it gives you error as modern c compilers blocked gets gcc gnu compiler collection
+    // gets(buffer);
+
+    // fgets expects and address in first argument and size of fget and array size should be same
+    fgets(buffer,sizeof(buffer),stdin);
+
+    printf("Buffer content: %s\n",buffer);
+
     return 0;
 }
